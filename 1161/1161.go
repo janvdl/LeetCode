874c556422046c64df41bd4963fb45612dcc7b43
Jan van der Linde <jvdl@jvdl.me>
2026-01-06 13:30:13 +0000

    Leetcode daily for 2025-01-06

---
+++ 1161/1161.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 7,
+			Left: &TreeNode{
+				Val: 7,
+			},
+			Right: &TreeNode{
+				Val: -8,
+			},
+		},
+		Right: &TreeNode{
+			Val: 0,
+		},
+	}
+
+	fmt.Println(maxLevelSum(&root))
+}
+
+func maxLevelSum(root *TreeNode) int {
+	levelSums := make(map[int]int)
+	traverseTree(root, 1, levelSums)
+
+	maxSum := math.Inf(-1)
+	maxDepth := math.Inf(-1)
+
+	for k, v := range levelSums {
+		if v > int(maxSum) {
+			maxSum = float64(v)
+			maxDepth = float64(k)
+		}
+	}
+
+	return int(maxDepth)
+}
+
+func traverseTree(node *TreeNode, depth int, levelSums map[int]int) {
+	if _, ok := levelSums[depth]; ok {
+		levelSums[depth] += node.Val
+	} else {
+		levelSums[depth] = node.Val
+	}
+
+	if node.Left != nil {
+		traverseTree(node.Left, depth+1, levelSums)
+	}
+	if node.Right != nil {
+		traverseTree(node.Right, depth+1, levelSums)
+	}
+}
